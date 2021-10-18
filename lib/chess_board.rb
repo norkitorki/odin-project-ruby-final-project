5c7e35f01e90ff3ab527cca368bacf321d25f8f6
norkitorki <norki@tuta.io>
2021-10-18 08:16:56 +0200

    Add to_s method to ChessBoard

--- lib/chess_board.rb
+++ lib/chess_board.rb
@@ -5,6 +5,30 @@ class ChessBoard
   attr_reader :fields
 
   def initialize
-    @fields = Array.new(8) { Array.new(8) }
+    @fields = Array.new(8) { Array.new(8, ' ') }
+  end
+
+  def to_s
+    top << rows << bottom
+  end
+
+  private
+
+  def rows
+    fields.reverse.map.with_index do |row, i|
+      "#{8 - i} ┃ #{row.join(' │ ')} ┃\n  "
+    end.join(row_seperator)
+  end
+
+  def row_seperator
+    '┠' << Array.new(8, '───').join('┼') << "┨\n"
+  end
+
+  def top
+    "  ┏━━━┯━━━┯━━━┯━━━┯━━━┯━━━┯━━━┯━━━┓\n"
+  end
+
+  def bottom
+    "┗━━━┷━━━┷━━━┷━━━┷━━━┷━━━┷━━━┷━━━┛\n    A   B   C   D   E   F   G   H\n"
   end
 end
