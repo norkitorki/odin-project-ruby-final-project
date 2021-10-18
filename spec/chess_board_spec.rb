4515f1b47a32b2f01ccf0737811260a1f2b1c952
norkitorki <norki@tuta.io>
2021-10-18 07:49:16 +0200

    Add to_s test to check for correct display of empty board

--- spec/chess_board_spec.rb
+++ spec/chess_board_spec.rb
@@ -4,4 +4,33 @@ require_relative '../lib/chess_board'
 
 describe ChessBoard do
   subject(:chess_board) { described_class.new }
+
+  describe '#to_s' do
+    context 'when fields are empty' do
+      it 'should print the empty board' do
+        expect(chess_board.to_s).to eq(
+          <<~BOARD
+            ┏━━━┯━━━┯━━━┯━━━┯━━━┯━━━┯━━━┯━━━┓
+          8 ┃   │   │   │   │   │   │   │   ┃
+            ┠───┼───┼───┼───┼───┼───┼───┼───┨
+          7 ┃   │   │   │   │   │   │   │   ┃
+            ┠───┼───┼───┼───┼───┼───┼───┼───┨
+          6 ┃   │   │   │   │   │   │   │   ┃
+            ┠───┼───┼───┼───┼───┼───┼───┼───┨
+          5 ┃   │   │   │   │   │   │   │   ┃
+            ┠───┼───┼───┼───┼───┼───┼───┼───┨
+          4 ┃   │   │   │   │   │   │   │   ┃
+            ┠───┼───┼───┼───┼───┼───┼───┼───┨
+          3 ┃   │   │   │   │   │   │   │   ┃
+            ┠───┼───┼───┼───┼───┼───┼───┼───┨
+          2 ┃   │   │   │   │   │   │   │   ┃
+            ┠───┼───┼───┼───┼───┼───┼───┼───┨
+          1 ┃   │   │   │   │   │   │   │   ┃
+            ┗━━━┷━━━┷━━━┷━━━┷━━━┷━━━┷━━━┷━━━┛
+              A   B   C   D   E   F   G   H
+          BOARD
+        )
+      end
+    end
+  end
 end
