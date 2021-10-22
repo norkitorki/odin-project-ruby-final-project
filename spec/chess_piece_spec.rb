677cb5741baf69f1fbbc78fcd39be94d263c3ee5
norkitorki <norki@tuta.io>
2021-10-22 11:58:17 +0200

    spec/chess_piece_spec.rb: Add tests for ChessPiece#to_vector and ChessPiece#to_coordinate

--- spec/chess_piece_spec.rb
+++ spec/chess_piece_spec.rb
@@ -58,4 +58,38 @@ describe ChessPiece do
       end
     end
   end
+
+  describe '#to_vector' do
+    context 'when the coordinate is valid' do
+      it 'should return the coordinate converted to a vector' do
+        valid_coordinate = :D5
+        vector = [4, 3]
+        expect(chess_piece.to_vector(valid_coordinate)).to eq(vector)
+      end
+    end
+
+    context 'when the coordinate is invalid' do
+      it 'should return nil' do
+        invalid_coordinate = 'A106'
+        expect(chess_piece.to_vector(invalid_coordinate)).to be_nil
+      end
+    end
+  end
+
+  describe '#to_coordinate' do
+    context 'when the vector is valid' do
+      it 'should return the vector converted to a coordinate' do
+        valid_vector = [7, 7]
+        coordinate = 'H8'
+        expect(chess_piece.to_coordinate(valid_vector)).to eq(coordinate)
+      end
+    end
+
+    context 'when the vector is invalid' do
+      it 'should return nil' do
+        invalid_vector = [10, 7]
+        expect(chess_piece.to_coordinate(invalid_vector)).to be_nil
+      end
+    end
+  end
 end
