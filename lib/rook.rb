76a352f80c00492f01d8b2a60bba92a58581b96d
norkitorki <norki@tuta.io>
2021-10-22 07:11:13 +0200

    lib/rook.rb: Add Rook class structure

--- lib/rook.rb
+++ lib/rook.rb
@@ -1,5 +1,28 @@
 # frozen-string-literal: true
 
+require_relative 'chess_piece'
+
 # Rook Chess Piece Class
-class Rook
+class Rook < ChessPiece
+  def moveset
+    return [] unless position
+
+    right_moves + down_moves + left_moves + up_moves
+  end
+
+  def right_moves
+    (file.next..'H').map { |file| ["#{file}#{rank}"] }
+  end
+
+  def down_moves
+    (rank.to_i - 1).downto(1).map { |rank| ["#{file}#{rank}"] }
+  end
+
+  def left_moves
+    ('A'...file).to_a.reverse.map { |file| ["#{file}#{rank}"] }
+  end
+
+  def up_moves
+    (rank.to_i + 1).upto(8).map { |rank| ["#{file}#{rank}"] }
+  end
 end
