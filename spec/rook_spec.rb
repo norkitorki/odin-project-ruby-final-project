08b5d0765d5eb3396033bb802a69a3791e2515fe
norkitorki <norki@tuta.io>
2021-10-22 07:34:29 +0200

    spec/rook_spec.rb: Add tests for Rook#moveset

--- spec/rook_spec.rb
+++ spec/rook_spec.rb
@@ -3,4 +3,38 @@
 require_relative '../lib/rook.rb'
 
 describe Rook do
+  subject(:rook) { Rook.new(unicode_rook) }
+  let(:unicode_rook) { '♖' }
+
+  describe '#moveset' do
+    context 'when position is nil' do
+      it 'should return an empty array' do
+        array = []
+        expect(rook.moveset).to eq(array)
+      end
+    end
+
+    context 'when a position is assigned' do
+      it 'should return the next valid moves from coordinate D4' do
+        position = 'D4'
+        rook.position = position
+        moves = [["E4"], ["F4"], ["G4"], ["H4"], ["D3"], ["D2"], ["D1"], ["C4"], ["B4"], ["A4"], ["D5"], ["D6"], ["D7"], ["D8"]]
+        expect(rook.moveset).to eq(moves)
+      end
+
+      it 'should return the next valid moves from coordinate A1' do
+        position = 'A1'
+        rook.position = position
+        moves = [["B1"], ["C1"], ["D1"], ["E1"], ["F1"], ["G1"], ["H1"], ["A2"], ["A3"], ["A4"], ["A5"], ["A6"], ["A7"], ["A8"]]
+        expect(rook.moveset).to eq(moves)
+      end
+
+      it 'should return the next valid moves from coordinate H8' do
+        position = 'H8'
+        rook.position = position
+        moves = [["H7"], ["H6"], ["H5"], ["H4"], ["H3"], ["H2"], ["H1"], ["G8"], ["F8"], ["E8"], ["D8"], ["C8"], ["B8"], ["A8"]]
+        expect(rook.moveset).to eq(moves)
+      end
+    end
+  end
 end
