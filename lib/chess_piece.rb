9ec941dd333c9ee5769d5de597e319299f9586ad
norkitorki <norki@tuta.io>
2021-10-22 11:21:19 +0200

    lib/chess_piece.rb: Add guard clause to #to_vector

--- lib/chess_piece.rb
+++ lib/chess_piece.rb
@@ -18,17 +18,19 @@ class ChessPiece
     end
   end
 
-  def valid_coordinate?(coordinate)
-    coordinate.to_s[/^[a-hA-H][1-8]$/] != nil
-  end
-
   # to_vector('D4') => [3, 3]
   def to_vector(coordinate)
+    return nil unless valid_coordinate?(coordinate)
+
     [coordinate[1].to_i - 1, coordinate[0].downcase.ord - 97]
   end
 
   # to_coordinate([3, 3]) => 'D4'
   def to_coordinate(vector)
     "#{('A'..'H').to_a[vector[0]]}#{vector[1] + 1}"
+  end
+
+  def valid_coordinate?(coordinate)
+    coordinate.to_s[/^[a-hA-H][1-8]$/] != nil
   end
 end
