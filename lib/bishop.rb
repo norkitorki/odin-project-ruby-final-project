fd4d239127cefc86cdf02cce106f3396e49af559
norkitorki <norki@tuta.io>
2021-10-25 09:00:14 +0200

    lib/bishop.rb: Add #moveset to Bishop

--- lib/bishop.rb
+++ lib/bishop.rb
@@ -4,6 +4,12 @@ require_relative 'chess_piece'
 
 # Bishop Chess Piece Class
 class Bishop < ChessPiece
+  def moveset
+    return [] unless position
+
+    left_down + left_up + right_down + right_up
+  end
+
   def left_down
     return [] unless position
 
