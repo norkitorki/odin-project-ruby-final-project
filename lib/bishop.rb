7c1d595c3c4a2c9da74d98d1ee87fb36066ccc28
norkitorki <norki@tuta.io>
2021-10-25 08:57:24 +0200

    lib/bishop.rb: Add #left_down to Bishop

--- lib/bishop.rb
+++ lib/bishop.rb
@@ -2,5 +2,14 @@
 
 require_relative 'chess_piece'
 
+# Bishop Chess Piece Class
 class Bishop < ChessPiece
+  def left_down
+    return [] unless position
+
+    l_rank = rank.to_i
+    moves = []
+    ('A'...file).to_a.reverse.each { |file| l_rank <= 1 ? break : moves << "#{file}#{l_rank -= 1}" }
+    moves
+  end
 end
