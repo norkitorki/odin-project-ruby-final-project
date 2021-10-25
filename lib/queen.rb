9d9aa577c3f3f371252af6a7880b6f271b12259d
norkitorki <norki@tuta.io>
2021-10-25 12:15:56 +0200

    lib/queen.rb: Add #right_up to Queen

--- lib/queen.rb
+++ lib/queen.rb
@@ -4,4 +4,12 @@ require_relative 'chess_piece'
 
 # Queen Chess Piece Class
 class Queen < ChessPiece
+  def right_up
+    return [] unless position
+
+    r_rank = rank.to_i
+    moves = []
+    (file.next..'H').each { |file, i| r_rank >= 8 ? break : moves << "#{file}#{r_rank += 1}" }
+    moves
+  end
 end
