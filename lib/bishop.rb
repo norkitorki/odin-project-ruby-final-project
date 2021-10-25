2ab6bf7e8daa6bfc71f26721acbd80b57ae9e07b
norkitorki <norki@tuta.io>
2021-10-25 08:58:11 +0200

    lib/bishop.rb: Add #left_up to Bishop

--- lib/bishop.rb
+++ lib/bishop.rb
@@ -12,4 +12,13 @@ class Bishop < ChessPiece
     ('A'...file).to_a.reverse.each { |file| l_rank <= 1 ? break : moves << "#{file}#{l_rank -= 1}" }
     moves
   end
+
+  def left_up
+    return [] unless position
+
+    l_rank = rank.to_i
+    moves = []
+    ('A'...file).to_a.reverse.each { |file| l_rank >= 8 ? break : moves << "#{file}#{l_rank += 1}" }
+    moves
+  end
 end
