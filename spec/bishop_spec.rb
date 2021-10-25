1fb832b40e35548d67c3ac75e9c8f036f32d2cad
norkitorki <norki@tuta.io>
2021-10-25 08:55:32 +0200

    spec/bishop_spec.rb: Add tests for Bishop#right_up

--- spec/bishop_spec.rb
+++ spec/bishop_spec.rb
@@ -80,4 +80,29 @@ describe Bishop do
       end
     end
   end
+
+  describe '#right_up' do
+    context 'when position is nil' do
+      it 'should return an empty array' do
+        empty_array = []
+        expect(bishop.right_up).to eq(empty_array)
+      end
+    end
+
+    context 'when a position is assigned' do
+      it 'should return the next valid moves from the right down from A3' do
+        position = 'A3'
+        bishop.position = position
+        moves = %w[B4 C5 D6 E7 F8]
+        expect(bishop.right_up).to eq(moves)
+      end
+
+      it 'should return an empty array when there are no valid moves' do
+        position = 'D8'
+        bishop.position = position
+        moves = []
+        expect(bishop.right_up).to eq(moves)
+      end
+    end
+  end
 end
