0bf4d2c08d2f95ffc63a3b162dc518ad7a3c4c1a
norkitorki <norki@tuta.io>
2021-10-25 12:23:44 +0200

    lib/queen.rb: Add #left to Queen

--- lib/queen.rb
+++ lib/queen.rb
@@ -38,4 +38,8 @@ class Queen < ChessPiece
     ('A'...file).to_a.reverse.each { |file| l_rank <= 1 ? break : moves << "#{file}#{l_rank -= 1}" }
     moves
   end
+
+  def left
+    position ? ('A'...file).to_a.reverse.map { |file| "#{file}#{rank}" } : []
+  end
 end
