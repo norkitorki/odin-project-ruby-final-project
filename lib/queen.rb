9e63a1a6617c83af04305f27e337510488d41956
norkitorki <norki@tuta.io>
2021-10-25 12:31:01 +0200

    lib/queen.rb: Add #moveset to Queen

--- lib/queen.rb
+++ lib/queen.rb
@@ -4,6 +4,12 @@ require_relative 'chess_piece'
 
 # Queen Chess Piece Class
 class Queen < ChessPiece
+  def moveset
+    return [] unless position
+
+    right_up + right + right_down + down + left_down + left + left_up + up
+  end
+
   def right_up
     return [] unless position
 
