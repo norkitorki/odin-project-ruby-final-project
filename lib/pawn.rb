a46038cde0faf9879d08438c15b557fe0f108a3f
norkitorki <norki@tuta.io>
2021-10-26 12:16:20 +0200

    lib/pawn.rb: Add #moveset to Pawn

--- lib/pawn.rb
+++ lib/pawn.rb
@@ -4,4 +4,9 @@ require_relative 'chess_piece'
 
 # Pawn Chess Piece Class
 class Pawn < ChessPiece
+  def moveset
+    return [] unless position
+
+    position[1] == '2' ? up(2) : up(1)
+  end
 end
