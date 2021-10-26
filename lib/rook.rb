edf7405324c541c9c194b09c11305834392a8254
norkitorki <norki@tuta.io>
2021-10-26 10:59:26 +0200

    lib/rook.rb: Remove #right_moves, #down_moves, #left_moves, #up_moves from Rook as their functionality is included in ChessTraversal module

--- lib/rook.rb
+++ lib/rook.rb
@@ -7,22 +7,6 @@ class Rook < ChessPiece
   def moveset
     return [] unless position
 
-    right_moves + down_moves + left_moves + up_moves
-  end
-
-  def right_moves
-    position ? (file.next..'H').map { |file| "#{file}#{rank}" } : []
-  end
-
-  def down_moves
-    position ? (rank.to_i - 1).downto(1).map { |rank| "#{file}#{rank}" } : []
-  end
-
-  def left_moves
-    position ? ('A'...file).to_a.reverse.map { |file| "#{file}#{rank}" } : []
-  end
-
-  def up_moves
-    position ? (rank.to_i + 1).upto(8).map { |rank| "#{file}#{rank}" } : []
+    right + down + left + up
   end
 end
