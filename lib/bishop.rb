74ffc791c40392493ade900bb4fbdbfdbde28769
norkitorki <norki@tuta.io>
2021-10-26 10:46:40 +0200

    lib/bishop.rb: Remove #left_down, #left_up, #right_down, #right_up from King as they are included in ChessTraversal module

--- lib/bishop.rb
+++ lib/bishop.rb
@@ -9,40 +9,4 @@ class Bishop < ChessPiece
 
     left_down + left_up + right_down + right_up
   end
-
-  def left_down
-    return [] unless position
-
-    l_rank = rank.to_i
-    moves = []
-    ('A'...file).to_a.reverse.each { |file| l_rank <= 1 ? break : moves << "#{file}#{l_rank -= 1}" }
-    moves
-  end
-
-  def left_up
-    return [] unless position
-
-    l_rank = rank.to_i
-    moves = []
-    ('A'...file).to_a.reverse.each { |file| l_rank >= 8 ? break : moves << "#{file}#{l_rank += 1}" }
-    moves
-  end
-
-  def right_down
-    return [] unless position
-
-    r_rank = rank.to_i
-    moves = []
-    (file.next..'H').each { |file| r_rank <= 1 ? break : moves << "#{file}#{r_rank -= 1}" }
-    moves
-  end
-
-  def right_up
-    return [] unless position
-
-    r_rank = rank.to_i
-    moves = []
-    (file.next..'H').each { |file| r_rank >= 8 ? break : moves << "#{file}#{r_rank += 1}" }
-    moves
-  end
 end
