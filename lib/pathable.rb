472a69c6b2d847096f0afe5370279ff5673f146e
norkitorki <norki@tuta.io>
2022-01-06 08:58:50 +0100

    Add lib/pathable.rb

---
+++ lib/pathable.rb
@@ -0,0 +1,26 @@
+# frozen-string-literal: true
+
+# Chess board paths for chess pieces
+module Pathable
+  private
+
+  PATH_MARKER = "\e[0;32;49m* \e[0m"
+
+  def add_path(moveset, chess_board)
+    moveset.each do |pos|
+      chess_board.place(pos, PATH_MARKER) if chess_board.field_empty?(pos)
+    end
+  end
+
+  def remove_path(moveset, chess_board)
+    moveset.each do |pos|
+      chess_board.remove(pos) if chess_board.at(pos) == PATH_MARKER
+    end
+  end
+
+  def display_path(moveset, chess_board)
+    add_path(moveset, chess_board)
+    puts chess_board
+    remove_path(moveset, chess_board)
+  end
+end
