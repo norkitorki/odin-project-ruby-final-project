4eb8ec0677a7b312f2345668013b6fff8946b1fd
norkitorki <norki@tuta.io>
2022-01-23 09:25:26 +0100

    lib/main.rb: Add ChessComputer functionality to game

--- lib/main.rb
+++ lib/main.rb
@@ -3,10 +3,11 @@
 require_relative 'chess'
 require_relative 'chess_player'
 require_relative 'chess_board'
+require_relative 'chess_computer'
 
-PLAYER1 = ChessPlayer.new('Player1')
-PLAYER2 = ChessPlayer.new('Player2')
+PLAYER1     = ChessPlayer.new('Player1')
+PLAYER2     = ChessPlayer.new('Player2')
 CHESS_BOARD = ChessBoard.new
+COMPUTER    = ChessComputer.new('Computer')
 
-CHESS = Chess.new(PLAYER1, PLAYER2, CHESS_BOARD)
-CHESS.play
+CHESS = Chess.new(PLAYER1, PLAYER2, CHESS_BOARD, COMPUTER).play
