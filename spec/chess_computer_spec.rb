7a1ba3862ef2d4132261840f8021ab08ebffad5e
norkitorki <norki@tuta.io>
2022-01-25 14:16:46 +0100

    spec/chess_computer_spec.rb: Add tests for #capture_move

--- spec/chess_computer_spec.rb
+++ spec/chess_computer_spec.rb
@@ -55,3 +55,46 @@ describe ChessComputer do
     end
   end
 
+  describe '#capture_move' do
+    let(:knight_piece) { chess_piece_class.new(:knight, 'G1') }
+    let(:computer_pieces) { [chess_piece_class.new(:pawn, 'E2'), knight_piece, chess_piece_class.new(:king, 'E1')] }
+    let(:opponent_pieces) { [chess_piece_class.new(:king, 'E8'), chess_piece_class.new(:queen, 'H3')] }
+
+    before { allow(computer).to receive(:pieces).and_return(computer_pieces) }
+
+    context 'when a chess piece can be captured' do
+      it 'should return the piece that should be moved' do
+        expect(computer.capture_move(opponent_pieces)[:piece]).to eq(knight_piece)
+      end
+
+      it 'should return the destination coordinate' do
+        expected_destination = 'H3'
+        expect(computer.capture_move(opponent_pieces)[:destination]).to eq(expected_destination)
+      end
+    end
+
+    context 'when no chess piece can be captured' do
+      before { knight_piece.update_position('F3') }
+
+      it 'should return nil' do
+        expect(computer.capture_move(opponent_pieces)).to eq(nil)
+      end
+    end
+
+    context "when the computers' pieces are empty" do
+      before { computer_pieces.clear }
+
+      it 'should return nil' do
+        expect(computer.capture_move(opponent_pieces)).to eq(nil)
+      end
+    end
+
+    context 'when opponent_pieces are empty' do
+      before { opponent_pieces.clear }
+
+      it 'should return nil' do
+        expect(computer.capture_move(opponent_pieces)).to eq(nil)
+      end
+    end
+  end
+
